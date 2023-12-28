50ade13df460780c2e3d691603f1aa164f3df0b6
jptwo676 <twobakabaka656@gmail.com>
2023-12-28 11:56:47 -0300

    done basic app class

--- class.py
+++ class.py
@@ -2,11 +2,13 @@ from tkinter import *
 from PIL import ImageTk,Image
 
 class App(Tk): 
-    def __init__(self):
+    def __init__(self, title, size):
         super().__init__()
-        self.title('img viewer')
-        #self.attributes('-type', 'dialog')
+        self.title(title)
+        self.attributes('-type', 'dialog')
+        self.geometry(f'{size[0]}x{size[1]}')
+        #self.minsize(600,600)
 
         self.mainloop()
 
-App()
+App('Image viewer', (600,600))
